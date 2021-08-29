11ec6af6064e3fb0f0a0b0c423132133e3f02458
cokoyoh <charlesokoyoh@gmail.com>
2021-08-29 09:31:13 +0300

    Consume the collection in index and add a test script which runs the dist/index.js file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+import { TodoItem } from './todoItem';
+import { TodoCollection } from './todoCollection';
+
+const todos: Array<TodoItem> = [
+  new TodoItem(1, 'Buy Flowers'),
+  new TodoItem(2, 'Get Shoes'),
+  new TodoItem(3, 'Collect Tickets'),
+  new TodoItem(4, 'Call Joe', true),
+]
+
+const collection: TodoCollection = new TodoCollection('Adam', todos);
+
 console.clear();
 
-console.log('Charles\' Todo List');
+console.log(`${collection.userName}'s Todo List`);
+
+const newId: number = collection.addTodo('Go for a run');
+
+const todoItem: TodoItem = collection.getTodoById(newId);
+
+todoItem.printDetails();
+
+// collection.addTodo(todoItem);
